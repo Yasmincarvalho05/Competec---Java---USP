78b6d7ded0f6648ec27c08426c2f55f7ad12ee5c
Yasmin Hillary Carvalho dos reis <yasmincarvalhoreis23@gmail.com>
2025-06-02 21:36:41 -0300

    Introdução while

---
+++ soma.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+    		int x=0;
+    		int soma = 0;
+    		while (x <=100){
+    		    soma = soma + x;
+    		    System.out.println(soma + " ");
+    		    x++;
+		        
+		}
+}
+}
